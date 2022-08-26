60ba82586c72efe6ec7f2c64ec380379bd6826ec
Caulfield Pearson <caulpearson@gmail.com>
2022-08-25 20:39:08 -0500

    queue message receiver

---
+++ QueueReceiver/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace QueueReceiver
+{
+    class Program
+    {
+        static string connectionString = "";
+        static string queueName = "caulfield-queue";
+        static ServiceBusClient client;
+        //processor that reads and processes messages from the queue
+        static ServiceBusProcessor processor;
+
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body}");
+
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        static async Task Main()
+        {
+            var clientOptions = new ServiceBusClientOptions() { TransportType = ServiceBusTransportType.AmqpWebSockets };
+            client = new ServiceBusClient(connectionString, clientOptions);
+
+            // create a processor that we can use to process the messages
+            processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+            try
+            {
+                // add handler to process messages
+                processor.ProcessMessageAsync += MessageHandler;
+
+                // add handler to process any errors
+                processor.ProcessErrorAsync += ErrorHandler;
+
+                // start processing 
+                await processor.StartProcessingAsync();
+
+                Console.WriteLine("Wait a sec, then press any key to end the processing");
+                Console.ReadKey();
+
+                // stop processing 
+                Console.WriteLine("\nStopping receiver...");
+                await processor.StopProcessingAsync();
+                Console.WriteLine("No longer receiving messages");
+            }
+            finally
+            {
+                // Cleaning up network resources and unmanaged objects
+                await processor.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+    }
+}
+
